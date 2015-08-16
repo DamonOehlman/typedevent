9f4a76ee05f1f8a0494f9e841525b57f60b9ea21
Damon Oehlman <damon.oehlman@gmail.com>
2015-08-16 20:52:58 +1000

    created extension tests

--- test/extend.js
+++ test/extend.js
@@ -1,18 +1,43 @@
 var test = require('tape');
 var TypedEvent = require('../event');
-var TestTypedEvent;
+var AnimalTypedEvent;
+var CatTypedEvent;
+var VehicleTypedEvent;
 
-test('can define a new type of event', function (t) {
+test('can extend TypedEvent -> VehicleTypedEvent', function (t) {
   t.plan(1);
-  TestTypedEvent = TypedEvent.extend();
-  t.ok(typeof TestTypedEvent == 'function', 'extended successfully');
+  VehicleTypedEvent = TypedEvent.extend();
+  t.ok(typeof VehicleTypedEvent == 'function', 'extended successfully');
 });
 
-test('can create an instance of the test typed event', function (t) {
+test('can extend TypedEvent -> AnimalTypedEvent', function (t) {
+  t.plan(1);
+  AnimalTypedEvent = TypedEvent.extend();
+  t.ok(typeof AnimalTypedEvent == 'function', 'extended successfully');
+});
+
+test('can create an instance of the AnimalTypedEvent', function (t) {
   var evt;
 
-  t.plan(2);
-  t.ok(evt = new TestTypedEvent());
-  t.ok(evt instanceof TestTypedEvent, 'is a TestTypedEvent');
+  t.plan(3);
+  t.ok(evt = new AnimalTypedEvent(), 'created AnimalTypedEvent');
+  t.ok(evt instanceof AnimalTypedEvent, 'is a AnimalTypedEvent');
   t.ok(evt instanceof TypedEvent, 'is a TypedEvent');
+});
+
+test('extend AnimalTypedEvent -> CatTypedEvent', function (t) {
+  t.plan(1);
+  CatTypedEvent = TypedEvent.extend(AnimalTypedEvent);
+  t.ok(typeof CatTypedEvent == 'function', 'extended');
+});
+
+test('an instance of CatTypedEvent is a CatTypedEvent, AnimalTypedEvent + TypedEvent', function (t) {
+  var evt;
+
+  t.plan(5);
+  t.ok(evt = new CatTypedEvent(), 'created CatTypedEvent');
+  t.ok(evt instanceof CatTypedEvent, 'is a CatTypedEvent');
+  t.ok(evt instanceof AnimalTypedEvent, 'is an AnimalTypedEvent');
+  t.ok(evt instanceof TypedEvent, 'is a TypedEvent');
+  t.notOk(evt instanceof VehicleTypedEvent, 'is not a VehicleTypedEvent');
 });
