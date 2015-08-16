a67af7413ddd6e7f48e63d4a85fcdd0f4f7309ef
Damon Oehlman <damon.oehlman@gmail.com>
2015-08-16 20:42:44 +1000

    started implementing tests

--- test/inherit-single.js
+++ test/inherit-single.js
@@ -1,7 +1,9 @@
 var test = require('tape');
-var TypedEvent = require('..');
+var TypedEvent = require('../event');
 var TestTypedEvent;
 
-test('can define a new type of event', function () {
-  TestTypedEvent = T
+test('can define a new type of event', function (t) {
+  t.plan(1);
+  TestTypedEvent = TypedEvent.extend();
+  t.ok(TestTypedEvent instanceof TypedEvent, 'extended successfully');
 });
